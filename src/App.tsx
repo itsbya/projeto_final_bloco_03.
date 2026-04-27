62bd0104bb10c5925a80530f354e3c906b69d85b
Ana Beatriz <anabeatrizcarvalhoesmaile@gmail.com>
2026-04-27 14:43:03 -0300

    Etapa 3: CRUD de Categorias

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,13 @@ import Home from './paginas/home/Home'
 import ListaCategorias from './componentes/categorias/ListaCategorias'
 import FormCategoria from './componentes/categorias/FormCategoria'
 import DeletarCategoria from './componentes/categorias/DeletarCategoria'
-import ListaProdutos from './componentes/produtos/ListaProdutos'
-import FormProduto from './componentes/produtos/FormProduto'
-import DeletarProduto from './componentes/produtos/DeletarProduto'
-import Carrinho from './paginas/carrinho/Carrinho'
-import DetalhesProduto from './paginas/produtos/DetalhesProduto'
 
 function App() {
   return (
     <BrowserRouter>
       <ToastContainer />
       <Navbar />
-      <div className='min-h-[80vh] bg-slate-50'>
+      <div className='min-h-[80vh]'>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
@@ -27,12 +22,6 @@ function App() {
           <Route path="/cadastrarCategoria" element={<FormCategoria />} />
           <Route path="/editarCategoria/:id" element={<FormCategoria />} />
           <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
-          <Route path="/produtos" element={<ListaProdutos />} />
-          <Route path="/cadastrarProduto" element={<FormProduto />} />
-          <Route path="/editarProduto/:id" element={<FormProduto />} />
-          <Route path="/deletarProduto/:id" element={<DeletarProduto />} />
-          <Route path="/detalhesProduto/:id" element={<DetalhesProduto />} />
-          <Route path="/carrinho" element={<Carrinho />} />
         </Routes>
       </div>
       <Footer />
